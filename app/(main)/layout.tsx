197fd89b8aa1329cde622cabb347857b060c495a
arkoroy05 <132930851+arkoroy05@users.noreply.github.com>
2024-12-04 20:01:07 +0530

    last comit

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { useConvexAuth } from "convex/react"
+
 import { redirect } from "next/navigation"
+import Navbar from "./_components/Navbar"
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth()
@@ -13,7 +15,12 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
     return redirect("/")
   }
 
-  return <div>{children}</div>
+  return <div className="h-full flex dark:bg-[#1F1F1F]">
+    <Navbar/>
+    <main className="flex-1 h-full overflow-y-auto">
+    {children}
+      </main>
+    </div>
 }
 
 export default MainLayout
